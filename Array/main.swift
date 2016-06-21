1cd7eb7c98ce7bafbc0ceb29f5822aa9c4bbd0ab
20141105052 <xiaoods3on1@sina.com>
2016-06-21 10:15:51 +0800

    swift-sort
    通过使用swift语句，实现数组中数据的冒泡排序，首先通过定义一个数组，设置想要排序的数据，利用for循环和if判断语句进行比较大小，最后使用f
    or循环输出遍历，得到由小到大排序的数据

--- Array/main.swift
+++ Array/main.swift
@@ -8,5 +8,28 @@
 
 import Foundation
 
-print("Hello, World!")
+var array = [4,7,3,11,65,34,76,98,654,354,234,123,34,5,6,87,121,235,980,144]
+
+
+
+
+         for var i = 0; i < array.count - 1; ++i {
+         for var j = 0; j < array.count - 1 - i; ++j{
+                 if array[j] > array[j + 1] {
+                 var temp = array[j + 1]
+                 array[j + 1] = array[j]
+                 array[j] = temp
+                     }
+             }
+     }
+
+
+
+ for item in array
+ {
+
+         print(item)
+     }
+
+
 
