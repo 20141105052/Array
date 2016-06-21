2784e58f192dbf9cfffd6f1943f3d7644169ff8c
20141105052 <xiaoods3on1@sina.com>
2016-06-21 08:22:06 +0800

    Initial Commit

---
+++ Array/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Array
+//
+//  Created by 20141105052 on 16/6/21.
+//  Copyright © 2016年 20141105052郭孝宇. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
